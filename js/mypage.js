7d2501e545f495ee51ce1b8d27386245e8fd8779
jeongdev <wkdgowjdpo@gmail.com>
2021-09-13 21:34:28 +0900

    my page 페이지 구현

---
+++ js/mypage.js
@@ -0,0 +1,40 @@
+
+$('.user_content1').click(function(){
+    if($('.click_div1').css("display")=="none"){
+        $('#down_btn').attr("src","up_icon.png");
+        $('.click_div1').css("display","block");
+    }else{
+        $('#down_btn').attr("src","down2_icon.png");
+        $('.click_div1').css("display","none");
+    }
+});
+
+$('.user_content2').click(function(){
+    if($('.click_div2').css("display")=="none"){
+        $('#down_btn').attr("src","up_icon.png");
+        $('.click_div2').css("display","block");
+    }else{
+        $('#down_btn').attr("src","down2_icon.png");
+        $('.click_div2').css("display","none");
+    }
+});
+
+$('.user_content3').click(function(){
+    if($('.click_div3').css("display")=="none"){
+        $('#down_btn').attr("src","up_icon.png");
+        $('.click_div3').css("display","block");
+    }else{
+        $('#down_btn').attr("src","down2_icon.png");
+        $('.click_div3').css("display","none");
+    }
+});
+
+$('.user_content4').click(function(){
+    if($('.click_div4').css("display")=="none"){
+        $('#down_btn').attr("src","up_icon.png");
+        $('.click_div4').css("display","block");
+    }else{
+        $('#down_btn').attr("src","down2_icon.png");
+        $('.click_div4').css("display","none");
+    }
+});
